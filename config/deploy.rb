1801fccae83220757d0f0947505298c84ec4021c
danielosbaldo <valenzuelaquintanadanielo@gmail.com>
2017-01-23 22:59:38 -0700

    rename deploy seerver

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,10 @@
 # config valid only for current version of Capistrano
 lock "3.7.1"
 
-set :application, 'contactbook'
+set :application, 'medicapp'
 set :repo_url, 'git@bitbucket.org:makingcode/medicappv2.git' # Edit this to match your repository
 set :branch, :master
-set :deploy_to, '/home/deploy/contactbook'
+set :deploy_to, '/home/deploy/medicapp'
 set :pty, true
 set :linked_files, %w{config/database.yml config/application.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
