3e19aabd2997eb41c2e094a698ddc0ef12595232
danielosbaldo <valenzuelaquintanadanielo@gmail.com>
2017-04-27 10:48:31 -0700

    deploy cchanges

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server '107.170.60.169', user: 'deploy', roles: %w{web app}, port:80
+server '107.170.60.169', user: 'deploy', roles: %w{web app}
 
 
 
