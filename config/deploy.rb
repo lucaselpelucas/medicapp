27c22e5e2b672c38063ffa0fbe8be5dc251a06c7
danielosbaldo <valenzuelaquintanadanielo@gmail.com>
2017-04-27 09:28:00 -0700

    Set puma change server

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,6 +4,7 @@ lock "3.7.1"
 set :application, 'medicapp'
 set :repo_url, 'git@github.com:danielosbaldo/medicapp.git' # Edit this to match your repository
 set :branch, :master
+set :user, 'deploy'
 set :deploy_to, '/home/deploy/medicapp'
 set :pty, true
 set :linked_files, %w{config/database.yml config/application.yml}
@@ -12,20 +13,32 @@ set :keep_releases, 1
 set :rvm_type, :user
 set :rvm_ruby_version, '2.3.1' # Edit this if you are using MRI Ruby
 
-set :puma_rackup, -> { File.join(current_path, 'config.ru') }
-set :puma_state, "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
-set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"    #accept array for multi-bind
-set :puma_conf, "#{shared_path}/puma.rb"
-set :puma_access_log, "#{shared_path}/log/puma_error.log"
-set :puma_error_log, "#{shared_path}/log/puma_access.log"
-set :puma_role, :app
-set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
-set :puma_threads, [0, 8]
-set :puma_workers, 0
+# Don't change these unless you know what you're doing
+set :pty,             true
+set :use_sudo,        false
+set :stage,           :production
+set :deploy_via,      :remote_cache
+set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :puma_preload_app, true
 set :puma_worker_timeout, nil
-set :puma_init_active_record, true
-set :puma_preload_app, false
+set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+
+## Defaults:
+# set :scm,           :git
+# set :branch,        :master
+# set :format,        :pretty
+# set :log_level,     :debug
+# set :keep_releases, 5
+
+## Linked Files & Directories (Default None):
+# set :linked_files, %w{config/database.yml}
+# set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
@@ -40,7 +53,16 @@ namespace :puma do
 end
 
 namespace :deploy do
-
+  desc "Make sure local git is in sync with remote."
+  task :check_revision do
+    on roles(:app) do
+      unless `git rev-parse HEAD` == `git rev-parse origin/master`
+        puts "WARNING: HEAD is not the same as origin/master"
+        puts "Run `git push` to sync changes."
+        exit
+      end
+    end
+  end
 
   desc 'Initial Deploy'
   task :initial do
@@ -57,6 +79,12 @@ namespace :deploy do
     end
   end
 
+  before :starting,     :check_revision
+  after  :finishing,    :compile_assets
   after  :finishing,    :cleanup
   after  :finishing,    :restart
 end
+
+# ps aux | grep puma    # Get puma pid
+# kill -s SIGUSR2 pid   # Restart puma
+# kill -s SIGTERM pid   # Stop puma
